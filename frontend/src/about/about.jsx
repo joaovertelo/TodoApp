39d5a73e72c4425e55d08765dd5aee127dd8bb22
João Marcos Vertelo Pimenta <joao.pimenta@groupsoftware.com.br>
2017-11-23 16:04:43 -0200

    TodoApp react sem redux

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2> Nossa História</h2>
+        <p> Teste teste etejk sdlfj asdfj sdfk </p>
+        <h2> Missão e visão</h2>
+        <p> Teste teste etejk sdlfj asdfj sdfk </p>
+    </div>
+)
